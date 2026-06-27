7a4a03536ed153c3b53087fb8c7a6a63fc8f3f26
timagineer <timagineer@proton.me>
2026-06-27 16:26:03 -0500

    Update footer to display current year
    Add current year to footer time element

--- src/date.js
+++ src/date.js
@@ -1,1 +1,5 @@
-
+const yr = document.querySelector('footer time');
+if (yr) yr.textContent = new Date().getFullYear().toString().slice(-2);
+// <footer>
+//     <span>&copy;&apos;<time></time> <a href="https://timagineer.com">Timagineer</a></span>
+// </footer>
