6ebde5d0081ea25854d9a5649f65058e4531b531
timagineer <timagineer@proton.me>
2026-06-28 20:21:04 -0500

    Fix footer comment formatting in date.js

--- src/date.js
+++ src/date.js
@@ -1,5 +1,7 @@
 const yr = document.querySelector('footer time');
 if (yr) yr.textContent = new Date().getFullYear().toString().slice(-2);
+
+// <script defer src=https://assets.timagineer.com/src/date.js></script>
 // <footer>
 //     <span>&copy;&apos;<time></time> <a href="https://timagineer.com">Timagineer</a></span>
 // </footer>
